f4e29135626030a6f1c36ced6c03070660e9fe17
TaranJoh <tarjoh51636@stud.noroff.no>
2024-11-23 18:15:14 +0100

    updates header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import { Navbar, Nav, Container } from "react-bootstrap";
+
 function Header() {
-  return <h1>Holidaze</h1>;
+  return (
+    <Navbar style={{ backgroundColor: "#566D8C" }} variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#home">Holidaze</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#browse">Browse</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#profile">Profile</Nav.Link>
+            <Nav.Link href="#logout">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
